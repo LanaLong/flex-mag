55c7ce778329e814095722d500aada1d403dae16
LanaLong <qmexperience@gmail.com>
2018-04-17 22:31:08 +0300

    FH: themeforest-12772303-flex-mag-responsive-wordpress-news-theme

---
+++ Child Theme/flex-mag-child/functions.php
@@ -0,0 +1,14 @@
+<?php
+function flex_mag_child_enqueue_styles() {
+
+    $parent_style = 'mvp-custom-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'mvp-custom-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'flex_mag_child_enqueue_styles' );
+?>
